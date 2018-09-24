379d79bce9241907df8d15047a9d2849637f101f
Yaroslav Bezhnar <yaroslav.bezhnar@yahoo.com>
2018-09-24 23:56:07 +0300

    added logger

---
+++ ParseFileApp/Logger.cs
@@ -0,0 +1,75 @@
+﻿#region
+using System;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+#endregion
+
+namespace ParseFileApp
+{
+    [Flags]
+    public enum MessageType
+    {
+        [Description( "Info" )]
+        Info,
+
+        [Description( "Warning" )]
+        Warning,
+
+        [Description( "Error" )]
+        Error
+    }
+
+    /// <summary>
+    ///     Represents a class for logging Errors/Warnings/Information.
+    /// </summary>
+    public static class Logger
+    {
+        #region Constants
+        private static readonly string _logTable;
+        private static readonly SqlConnection _connection;
+        #endregion
+
+        #region Constructors
+        static Logger()
+        {
+            _logTable = "LogInfo";
+            _connection = new SqlConnection( SentenceManager.DB_CONNECTION );
+        }
+        #endregion
+
+        #region Public Methogs
+        /// <summary>
+        ///     Write logging information to file.
+        /// </summary>
+        /// <param name="message">Message to write.</param>
+        /// <param name="messageType">Type of message.</param>
+        public static void WriteLog( string message, MessageType messageType = MessageType.Info )
+        {
+            try
+            {
+                checkConnection();
+
+                using ( var command = new SqlCommand( $"INSERT INTO {_logTable} (Time, Message, Type) VALUES ('{DateTime.Now}', '{message}', '{messageType}')", _connection ) )
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+        #endregion
+
+        #region Private Methods
+        private static void checkConnection()
+        {
+            if ( _connection != null && _connection.State != ConnectionState.Open )
+            {
+                _connection.Open();
+            }
+        }
+        #endregion
+    }
+}
