fbf667d6cad4dd1b51bc0805fedfb69bc460d54a
Yaroslav Bezhnar <yaroslav.bezhnar@yahoo.com>
2018-09-25 11:30:34 +0300

    added scripts for db

---
+++ Create dbo.LogInfo.sql
@@ -0,0 +1,18 @@
+﻿USE [MyDataBase]
+GO
+
+/****** Object: Table [dbo].[LogInfo] Script Date: 9/25/18 11:27:20 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[LogInfo] (
+    [Id]      INT          IDENTITY (1, 1) NOT NULL,
+    [Time]    DATETIME     NOT NULL,
+    [Message] TEXT         NOT NULL,
+    [Type]    VARCHAR (20) NOT NULL
+);
+
+
---
+++ Create dbo.Sentences.sql
@@ -0,0 +1,17 @@
+﻿USE [MyDataBase]
+GO
+
+/****** Object: Table [dbo].[Sentences] Script Date: 9/25/18 11:28:09 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Sentences] (
+    [Id]          INT  IDENTITY (1, 1) NOT NULL,
+    [Sentence]    TEXT NOT NULL,
+    [Occurrences] INT  NOT NULL
+);
+
+
